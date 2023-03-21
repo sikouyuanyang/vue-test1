7151c070f0748c81fd6abcb830c9482cda06df6f
kouyuanyang <1697315180@qq.com>
2023-03-21 09:09:38 +0800

    每隔3秒获取一次

--- src/views/test/Test1.vue
+++ src/views/test/Test1.vue
@@ -14,16 +14,23 @@
             }
         },
         mounted(){
-            axios({
-                method:"get",
-                // url:"http://iwenwiki.com/api/blueberrypai/getChengpinDetails.php"
-                url:"http://127.0.0.1:8000/test1/hello_world/"
-            }).then(res => {
-                // console.log(res.data.chengpinDetails[0])
-                // this.temperature = res.data.chengpinDetails[0]
-                console.log(res.data.wsdcgqs[0])
-                this.wsdcgq1 = res.data.wsdcgqs[0]
-            })
+            // GetWdSd2(){
+                setInterval(this.GetWdSd1,3000);
+            // }
         },
+        methods:{
+            GetWdSd1(){
+                axios({
+                    method:"get",
+                    // url:"http://iwenwiki.com/api/blueberrypai/getChengpinDetails.php"
+                    url:"http://127.0.0.1:8000/test1/hello_world/"
+                }).then(res => {
+                    // console.log(res.data.chengpinDetails[0])
+                    // this.temperature = res.data.chengpinDetails[0]
+                    console.log(res.data.wsdcgqs[0])
+                    this.wsdcgq1 = res.data.wsdcgqs[0]
+                })
+            }
+        }
     }
 </script>
